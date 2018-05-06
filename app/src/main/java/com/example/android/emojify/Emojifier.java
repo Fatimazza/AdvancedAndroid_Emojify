6787c3b5036ed391594150f86825b733cd9ffafc
Fatima Azzahro <fatima@paypro.id>
2018-05-06 14:57:02 +0700

    [Exercise 5] Overlay Emoji 1 - Create Bitmap variable, Move whichEmoji() inside Switch statement

--- app/src/main/java/com/example/android/emojify/Emojifier.java
+++ app/src/main/java/com/example/android/emojify/Emojifier.java
@@ -68,11 +68,16 @@ class Emojifier {
             // Iterate through the faces
             for (int i = 0; i < faces.size(); ++i) {
                 Face face = faces.valueAt(i);
-                // Get the appropriate emoji for each face
-                whichEmoji(face);
 
-                // TODO (4): Create a variable called emojiBitmap to hold the appropriate Emoji bitmap and remove the call to whichEmoji()
+                // COMPLETED (4): Create a variable called emojiBitmap to hold the appropriate Emoji bitmap and remove the call to whichEmoji()
+                Bitmap emojiBitmap;
+
+
                 // TODO (5): Create a switch statement on the result of the whichEmoji() call, and assign the proper emoji bitmap to the variable you created
+                switch (whichEmoji(face)) {
+
+                }
+
                 // TODO (8): Call addBitmapToFace(), passing in the resultBitmap, the emojiBitmap and the Face  object, and assigning the result to resultBitmap
 
             }
